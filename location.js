6e01dfc38b17d25500510c55f95042f07f5427fc
Francisco Treacy <francisco.treacy@gmail.com>
2010-04-10 15:50:50 +0200

    first commit

---
+++ location.js
@@ -0,0 +1,41 @@
+var sys = require('sys'),
+  http = require('http'),
+  fs = require('fs'),
+  file = __dirname + '/' + "visits";
+
+sys.exec("touch " + file);
+var hostip = http.createClient(80, "api.hostip.info");
+
+http.createServer(function (request, response) {
+  response.sendHeader('Content-Type: image/gif');
+  var ip = request.connection.remoteAddress;
+  // response.write(ip);
+  log(ip, request.headers['user-agent']);
+  response.close();
+}).listen(4080);
+
+function log(ip, browser) {
+  var req = hostip.request("GET", "/get_html.php?ip=" + ip, {host: "api.hostip.info"});
+  req.addListener('response', function (resp) {
+    var buffer = "";
+    resp.addListener("data", function (chunk) {
+      buffer += chunk;
+    });
+    resp.addListener("end", function () {
+      if (resp.statusCode < 300) {
+        var cc = buffer.split('\n');
+        var country = /Country\:\s(.*)\((\w\w)\)/g.exec(cc[0]);
+        var city = /City\:\s(.*)/g.exec(cc[1]);
+        var obj = { date: new Date(), ip: ip, country: country[1].trim(), code: country[2].trim(), city: city[1].trim(), browser: browser }
+        save(obj);
+      }
+    });
+  });
+  req.close();
+}
+
+function save(obj) {
+  fs.readFile(file, function(err, data) {
+    if (!err) fs.writeFile(file, JSON.stringify(obj) + "\n" + data);
+  });
+}
